1d99008531e6554bb4805ed03ad4afbd2e2b82f7
Samuk <samukcarlos@gmail.com>
2023-06-10 14:17:22 -0300

    ex completo

--- rotas-ex1/src/components/Header/index.tsx
+++ rotas-ex1/src/components/Header/index.tsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 export default function Header(){
     return(
 
         <header>
-            <div className='container'>
+            <div className='header-content-conteiner container'>
                 <Link to="/">
                     <h1 className="site-name">MeuSite</h1>
-                </Link>               
+                </Link>       
+                <nav className='navBar'>
+                    <NavLink to="/home" className={({isActive})=>isActive ? 'menu-item menu-active':'menu-item'}>
+                        Início
+                    </NavLink  >
+                    <NavLink to="/promotion" className={({isActive})=>isActive ? 'menu-item menu-active':'menu-item'}>
+                        Promoção
+                    </NavLink >
+                    <NavLink to="/subscription" className={({isActive})=>isActive ? 'menu-item menu-active':'menu-item'}>
+                        Participar
+                    </NavLink >
+                </nav>        
             </div>
         </header>
         
